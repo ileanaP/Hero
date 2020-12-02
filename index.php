0256ef859de513d204a9b19bc683d92709ed91f2
IleanaP <ileana.profeanu@gmail.com>
2020-12-02 10:44:21 +0200

    deleted unused lines from index

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 require_once('loader.php');
 
-/*$home = new classes\hero();
-print_r($home->write());
-echo '<br/><br/><br/>';*/
-
 $hero = new classes\Hero($HeroStats);
-//$hero->write();
-echo '<br/><br/><br/><br/>';
+
 $beast = new classes\Beast($BeastStats);
-//$beast->write();
 
 $battle = new classes\Battle($hero, $beast);
 
