e4acbc103637a25d2b8d24a9cb352b26e0b1d0c4
IleanaP <ileana.profeanu@gmail.com>
2020-12-02 11:09:17 +0200

    code cleanup

--- classes/CreatureStatsBuilder.php
+++ classes/CreatureStatsBuilder.php
@@ -7,10 +7,4 @@ interface CreatureStatsBuilder
 	public function setStats();
 
 	public function getResult();
-
-	/*public function setHealth($health)   { $_health = intval($health);   }
-	public function setHealth($strength) { $_health = intval($strength); }
-	public function setHealth($defence)  { $_health = intval($defence);  }
-	public function setHealth($speed)    { $_health = intval($speed);    }
-	public function setHealth($luck)     { $_health = intval($luck);     }*/
 }
